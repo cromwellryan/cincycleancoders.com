6e809bce109920d22209b1f9e17f9461a0fae5cc
Ryan Cromwell <cromwellryan@hotmail.com>
2011-03-22 22:52:20 -0400

    skeleton site running on webby with cap stuff

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+default_run_options[:pty] = true
+
+set :ssh_options, {:forward_agent => true}
+
+set :user, 'cincycleancode'
+set :domain, 'cincycleancoders.com'
+set :application, 'cincycleancoders'
+
+set :repository,  "git@github.com:cromwellryan/cincycleancoders.com.git"
+set :deploy_to, "/home/#{user}/webapps/#{application}"
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
+
+server domain, :app, :web
+role :db, domain, :primary => true
+
+after :deploy, "webby:deploy"
+
+namespace :webby do  
+  desc "Generate the webby site"  
+  task :deploy do  
+    run("cd #{deploy_to}/current && webby")
+  end
+end
